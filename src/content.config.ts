2e6876e6a36dc03d658d3eae3feaf1526849b2cf
TobysNap <naptobys17@gmail.com>
2025-02-06 22:00:49 -0300

    Agregar configuración de contenido y funciones utilitarias

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+import { glob } from 'astro/loaders';
+import { defineCollection, z } from 'astro:content';
+
+const experiences = defineCollection({
+	loader: glob({pattern: "**/*.md", base: './src/content/experiences'}),
+	schema: z.object({
+		company: z.string(),
+		date: z.string(),
+		role: z.string(),
+		achievements: z.array(
+			z.object({ header: z.string(), description: z.string() })
+		),
+		features: z.array(z.string()),
+		tags: z.array(z.string()),
+		url: z.string().optional(),
+	}),
+});
+
+export const collections = { experiences };
