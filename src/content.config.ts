3e4151c1895faafe70d33f7c14d8e5fbd224dcfc
TobysNap <naptobys17@gmail.com>
2025-08-11 18:49:03 -0300

    🔄️ feature: new content collecctions schemas

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const experiences = defineCollection({
-	loader: glob({pattern: "**/*.md", base: './src/content/experiences'}),
+	loader: glob({ pattern: '**/*.md', base: './src/content/experiences' }),
 	schema: z.object({
 		company: z.string(),
 		date: z.string(),
@@ -11,9 +11,39 @@ const experiences = defineCollection({
 			z.object({ header: z.string(), description: z.string() })
 		),
 		features: z.array(z.string()),
-		tags: z.array(z.string()),
 		infoUrl: z.string().optional(),
 	}),
 });
 
-export const collections = { experiences };
+const projects = defineCollection({
+	loader: glob({ pattern: '**/*.md', base: './src/content/projects' }),
+	schema: z.object({
+		title: z.string(),
+		subtitle: z.string(),
+		description: z.string(),
+		summary: z.string(),
+		keyTech: z.array(z.string()),
+		mainImg: z.string(),
+		imgs: z.array(
+			z.object({
+				image: z.string(),
+				caption: z.string(),
+				alt: z.string(),
+			})
+		),
+		stack: z.array(z.string()),
+		features: z.array(z.string()),
+		impact: z.array(
+			z.object({
+				title: z.string(),
+				subtitle: z.string(),
+			})
+		),
+		role: z.string(),
+		challenges: z.array(z.string()),
+		goal: z.string(),
+		demo: z.string().url().optional(),
+	}),
+});
+
+export const collections = { experiences, projects };
